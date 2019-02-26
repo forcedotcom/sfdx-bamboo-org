f0919166b352cc413228950ceb1f3b8b3aca2a0e
Emily Kapner <ekapner@salesforce.com>
2019-02-26 11:56:53 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 REM
-REM Download the SFDX CLI and install it
+REM Download Salesforce CLI and install it
 REM
 
 REM Decrypt server key
 openssl aes-256-cbc -d -md md5 -in assets/server.key.enc -out assets/server.key -k %bamboo_SERVER_KEY_PASSWORD%
 
-REM Setup SFDX environment variables
+REM Set up SFDX environment variables
 set SFDX_AUTOUPDATE_DISABLE="false"
 set SFDX_USE_GENERIC_UNIX_KEYCHAIN="true"
 set SFDX_DOMAIN_RETRY="300"
@@ -14,7 +14,7 @@ set SFDX_LOG_LEVEL="DEBUG"
 set DEPLOYDIR="src"
 set TESTLEVEL="RunLocalTests"
 
-REM Output SFDX version and plugin information
+REM Output CLI version and plugin information
 sfdx --version
 sfdx plugins --core
 
@@ -22,11 +22,11 @@ REM
 REM Deploy metadata to Salesforce
 REM
 
-REM Authenticate to Salesforce using server key
+REM Authenticate to Salesforce using the server key
 sfdx force:auth:jwt:grant --instanceurl https://test.salesforce.com --clientid %bamboo_SF_CONSUMER_KEY% --jwtkeyfile assets/server.key --username %bamboo_SF_USERNAME% --setalias UAT 
 
 REM Deploy metadata and execute unit tests
 sfdx force:mdapi:deploy --wait 10 --deploydir %DEPLOYDIR% --targetusername UAT --testlevel %TESTLEVEL%
 
-REM Example shows how to run a check only deploy
+REM Example shows how to run a check-only deploy
 REM sfdx force:mdapi:deploy --checkonly --wait 10 --deploydir %DEPLOYDIR% --targetusername UAT --testlevel %TESTLEVEL%
